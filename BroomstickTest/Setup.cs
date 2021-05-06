f4c793cbe3f2e983975e610a260ec54353f70784
Zniv <lt.zniv@gmail.com>
2021-05-06 18:46:47 +0200

    Broomstick kickoff

---
+++ BroomstickTest/Setup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using System.Reflection;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra;
+
+namespace BroomstickTest
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        [OneTimeSetUp]
+        public void DoSetup()
+        {
+            Log.DebugDelegate += Output;
+            Log.WarningDelegate += Output;
+            Log.ErrorDelegate += Output;
+
+            // Tell the engine about our mod assembly so it can load up our code.
+            // It doesn't matter which type as long as it comes from the mod's assembly.
+            // var a = Assembly.GetAssembly(typeof(MigrantCoderCharacterCardController)); // replace with your own type
+            //ModHelper.AddAssembly("Broomstick", a); // replace with your own namespace
+        }
+
+        protected void Output(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ BroomstickTest/Test.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using Handelabra.Sentinels.UnitTest;
+using System.Collections.Generic;
+
+namespace BroomstickTest
+{
+    [TestFixture()]
+    public class Test : BaseTest
+    {
+        [Test()]
+        public void TestModWorks()
+        {
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(env);
+
+
+            StartGame();
+
+            AssertIsInPlay("SmashBackField");
+
+            // Always deals 5 psychic to non villains!
+            PlayCard("FireEverything");
+
+            QuickHPCheck(0, -6); // Nemesis!
+
+            PlayTopCard(env);
+
+            // Deals 1 damage
+            QuickHPCheck(-1, -1);
+
+            // Heals 1 at the start of the environment turn
+            GoToStartOfTurn(env);
+            QuickHPCheck(1, 1);
+        }
+
+
+        [Test()]
+        public void TestBunkerVariant()
+        {
+            SetupGameController("BaronBlade", "Bunker/Broomstick.WaywardBunkerCharacter", "Megalopolis");
+
+            StartGame();
+
+            Assert.IsTrue(bunker.CharacterCard.IsPromoCard);
+            Assert.AreEqual("WaywardBunkerCharacter", bunker.CharacterCard.PromoIdentifierOrIdentifier);
+            Assert.AreEqual(30, bunker.CharacterCard.MaximumHitPoints);
+
+            GoToUsePowerPhase(bunker);
+
+            // Use the power, it draws 2 cards not 1!
+            QuickHandStorage(bunker);
+            UsePower(bunker);
+            QuickHandCheck(2);
+        }
+    }
+}
